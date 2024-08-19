8446faecd50381dd47e691a5e064680bae7d628b
Greg Gavelis <zoark0@gmail.com>
2024-08-19 11:06:59 -0400

    added Part2

---
+++ Part2/3a_submit.pbs.sh
@@ -0,0 +1,24 @@
+#PBS -N launch_nextflow_pipeline
+#PBS -M ggavelis@bigelow.org
+#PBS -m bea -j oe -o status_nextflow.pbs
+#PBS -l ncpus=1,mem=50MB,walltime=120:00:00
+
+echo "launching nextflow pipeline"
+
+cd /mnt/scgc_nfs/lab/ggavelis/analysis/siavash_hgt/aug9_second_round/
+
+module use /mnt/scgc_nfs/opt/modulefiles/common/
+module load anaconda/2.1.0
+module load singularity
+
+export NXF_EXECUTOR=pbspro
+export SINGULARITY_CACHEDIR="/mnt/scgc_nfs/containers"
+export SINGULARITY_TMPDIR="/mnt/scgc_nfs/containers/temp"
+export NXF_SINGULARITY_CACHEDIR="/mnt/scgc_nfs/containers"
+export NXF_CACHE_DIR="/mnt/scgc/scgc_temp/nextflow"
+export NXF_CONDA_ENABLED=true
+export NXF_VER=21.10.4
+
+NXF_VER=21.10.4 nextflow run 3b_siavash_hgt_pt2_pipeline.nf -profile charlie
+
+echo "done"
